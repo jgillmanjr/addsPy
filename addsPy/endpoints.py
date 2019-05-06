584c2855337d52fdab65af542de0b71f43d4384a
Jason Gillman Jr <jgillman@liquidweb.com>
2019-05-06 17:58:48 -0400

    Work in progress for TAFs

--- addsPy/endpoints.py
+++ addsPy/endpoints.py
@@ -640,3 +640,284 @@ pirep = {
         },
     },
 }
+
+taf = {
+    'request_index': {
+        'location': 'request_index',
+        'cdata_type': int,
+        'attributes': None,
+        'multi_occurs': False,
+        'children': None
+    },
+    'data_source': {
+        'location': 'data_source',
+        'cdata_type': None,
+        'attributes': [
+            ('name', str),
+        ],
+        'multi_occurs': False,
+        'children': None,
+    },
+    'request': {
+        'location': 'request',
+        'cdata_type': None,
+        'attributes': [
+            ('type', str),
+        ],
+        'multi_occurs': False,
+        'children': None,
+    },
+    'errors': {
+        'location': 'errors',
+        'cdata_type': None,
+        'attributes': None,
+        'multi_occurs': False,
+        'children': {
+            'error': {
+                'location': 'error',
+                'cdata_type': str,
+                'attributes': None,
+                'multi_occurs': True,
+                'children': None
+            },
+        },
+    },
+    'warnings': {
+        'location': 'warnings',
+        'cdata_type': None,
+        'attributes': None,
+        'multi_occurs': False,
+        'children': {
+            'warning': {
+                'location': 'warning',
+                'cdata_type': str,
+                'attributes': None,
+                'multi_occurs': True,
+                'children': None
+            },
+        },
+    },
+    'time_taken_ms': {
+        'location': 'time_taken_ms',
+        'cdata_type': int,
+        'attributes': None,
+        'multi_occurs': False,
+        'children': None,
+    },
+    'data': {
+        'location': 'data',
+        'cdata_type': None,
+        'attributes': [
+            ('num_results', int),
+        ],
+        'multi_occurs': False,
+        'children': {
+            'TAF': {
+                'location': 'TAF',
+                'cdata_type': None,
+                'attributes': None,
+                'multi_occurs': True,
+                'children': {
+                    'raw_text': {
+                        'location': 'raw_text',
+                        'cdata_type': str,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'station_id': {
+                        'location': 'station_id',
+                        'cdata_type': str,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'issue_time': {
+                        'location': 'issue_time',
+                        'cdata_type': utc_timestamp,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'bulletin_time': {
+                        'location': 'bulletin_time',
+                        'cdata_type': utc_timestamp,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'valid_time_from': {
+                        'location': 'valid_time_from',
+                        'cdata_type': utc_timestamp,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'valid_time_to': {
+                        'location': 'valid_time_to',
+                        'cdata_type': utc_timestamp,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'latitude': {
+                        'location': 'latitude',
+                        'cdata_type': Decimal,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'longitude': {
+                        'location': 'longitude',
+                        'cdata_type': Decimal,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'elevation_m': {
+                        'location': 'elevation_m',
+                        'cdata_type': Decimal,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'forecast': {
+                        'location': 'forecast',
+                        'cdata_type': None,
+                        'attributes': None,
+                        'multi_occurs': True,
+                        'children': {
+                            'fcst_time_from':{
+                                'location': 'fcst_time_from',
+                                'cdata_type': utc_timestamp,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                            'fcst_time_to':{
+                                'location': 'fcst_time_to',
+                                'cdata_type': utc_timestamp,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                            'change_indicator': {
+                                'location': 'change_indicator',
+                                'cdata_type': str,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                        }
+                    },
+                    'wind_dir_degrees': {
+                        'location': 'wind_dir_degrees',
+                        'cdata_type': int,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'wind_speed_kt': {
+                        'location': 'wind_speed_kt',
+                        'cdata_type': int,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'wind_gust_kt': {
+                        'location': 'wind_gust_kt',
+                        'cdata_type': int,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'visibility_statute_mi': {
+                        'location': 'visibility_statute_mi',
+                        'cdata_type': Decimal,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'altim_in_hg': {
+                        'location': 'altim_in_hg',
+                        'cdata_type': Decimal,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'sea_level_pressure_mb': {
+                        'location': 'sea_level_pressure_mb',
+                        'cdata_type': Decimal,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': None
+                    },
+                    'quality_control_flags': {
+                        'location': 'quality_control_flags',
+                        'cdata_type': None,
+                        'attributes': None,
+                        'multi_occurs': False,
+                        'children': {
+                            'corrected': {
+                                'location': 'corrected',
+                                'cdata_type': out_bool,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                            'auto': {
+                                'location': 'auto',
+                                'cdata_type': out_bool,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                            'auto_station': {
+                                'location': 'auto_station',
+                                'cdata_type': out_bool,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                            'maintenance_indicator_on': {
+                                'location': 'maintenance_indicator_on',
+                                'cdata_type': out_bool,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                            'no_signal': {
+                                'location': 'no_signal',
+                                'cdata_type': out_bool,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                            'lightning_sensor_off': {
+                                'location': 'lightning_sensor_off',
+                                'cdata_type': out_bool,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                            'freezing_rain_sensor_off': {
+                                'location': 'freezing_rain_sensor_off',
+                                'cdata_type': out_bool,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                            'present_weather_sensor_off': {
+                                'location': 'present_weather_sensor_off',
+                                'cdata_type': out_bool,
+                                'attributes': None,
+                                'multi_occurs': False,
+                                'children': None
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+}
